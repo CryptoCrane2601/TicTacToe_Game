eced379faa04fbdc7978e6a030f92d0ab2581e49
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2021-04-06 00:05:20 +0200

    First commit

---
+++ TicTacToeGame.py
@@ -0,0 +1,17 @@
+# TicTacToe Game
+
+def displayBoard(row1,row2,row3,row4,row5):
+    print(row1)
+    print(row2)
+    print(row3)
+    print(row4)
+    print(row5)
+
+row1 = " ' ' | ' ' | ' ' "
+row2 = "-----------------"
+row3 = " ' ' | ' ' | ' ' "
+row4 = "-----------------"
+row5 = " ' ' | ' ' | ' ' "
+
+displayBoard(row1,row2,row3,row4,row5) 
+    
