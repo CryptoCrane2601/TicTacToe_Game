6af7b6aef1893c5e80b0865266d85e0f94ef80ed
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2021-04-06 12:13:09 +0200

    Added function for postion change

--- TicTacToeGame.py
+++ TicTacToeGame.py
@@ -1,5 +1,6 @@
 # TicTacToe Game
 
+#Display function 
 def displayBoard(row1,row2,row3,row4,row5):
     print(row1)
     print(row2)
@@ -7,11 +8,29 @@ def displayBoard(row1,row2,row3,row4,row5):
     print(row4)
     print(row5)
 
-row1 = " ' ' | ' ' | ' ' "
-row2 = "-----------------"
-row3 = " ' ' | ' ' | ' ' "
-row4 = "-----------------"
-row5 = " ' ' | ' ' | ' ' "
+row1 = ["   ,|    ,|    "]
+row2 =  "---------------"
+row3 = ["   ,|    ,|    "]
+row4 = "----------------"
+row5 = ["   ,|    ,|    "]
 
-displayBoard(row1,row2,row3,row4,row5) 
+displayBoard(row1,row2,row3,row4,row5)
+
+#Position function
+
+
+
+def position_change():
+
+    choice = 'Wrong'
+
+    while choice not in ['0','1','2']:
+
+        choice = input('Pick a positionn in (0,1,2): ')
+
+        if choice not in ['0','1','2']:
+            print('Sorry, invalid choice!')
+
+        return int(choice)
+    
     
